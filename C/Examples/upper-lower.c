7475c6f905005fe6caf5f507f85605a56220130f
Dayeong Kang <62553200+tula3and@users.noreply.github.com>
2020-11-30 16:01:46 +0900

    Update upper-lower.c

--- C/Examples/upper-lower.c
+++ C/Examples/upper-lower.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <string.h>
+//#include <ctype.h>
 
 int main() {
 	char string[100];
@@ -12,9 +13,11 @@ int main() {
 	while (i < strlen(string)) {
 		if (string[i] >= 'A' && string[i] <= 'Z') {
 			string[i] += 32;
+			//string[i] = tolower(string[i]);
 		}
 		else if (string[i] >= 'a' && string[i] <= 'z') {
 			string[i] -= 32;
+			//string[i] = toupper(string[i]);
 		}
 
 		i += 1;
